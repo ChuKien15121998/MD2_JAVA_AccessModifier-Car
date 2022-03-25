19c1efaaa0677756b085f905a201d383ac31cd29
ChuKien15121998 <chuvankien151298@gmail.com>
2022-03-25 10:50:59 +0700

    Done

---
+++ src/com/company/Circle.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return Math.PI * this.radius + this.radius;
+    }
+}
